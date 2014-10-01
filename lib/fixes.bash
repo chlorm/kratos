7554c89e315dc72cf34ae5be5401f2563f855ae2
codyopel <codyopel@gmail.com>
2014-09-30 21:29:43 -0400

    minor syntax changes

--- lib/fixes.bash
+++ lib/fixes.bash
@@ -1,22 +1,17 @@
 #!/usr/bin/env bash
 
-# Creates an array from the name of the variable and values
+##[creates an array from the name of the variable and values]
 array_from_str () {
   ([ "$#" -lt "1" ] || [ "$#" -gt "2" ]) && return 1
-
-  # Read in the array data
-  local DATA
-  if [ "$#" -eq "1" ]; then
-    DATA="$(cat -)"
-  else
-    DATA="$2"
-  fi
-
-  # Convert the data into an array
+  ##[read in the array data]
+  local DATA;
+  if [ "$#" -eq "1" ];then DATA="$(cat -)"
+  else DATA="$2";fi
+  ##[convert the data into an array]
   eval "$1=($DATA)"
 }
 
-# Unique Array Functions with indicies starting at 0
+##[unique array functions with indicies starting at 0]
 array_append () {
   eval "$1=(\"\${$1[@]}\" \"$2\")"
 }
@@ -31,7 +26,7 @@ array_size () {
   eval "echo \${#$1[@]}"
 }
 
-# Tests to see if a binary exists in the path
+##[tests to see if a binary exists in the path]
 path_hasbin () {
   [ "$#" -ne "1" ] && return 2
   type $1 >/dev/null 2>&1
