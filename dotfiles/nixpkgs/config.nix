9f640e8b6bd4f6b50084c481c80673a65a1eb2e6
codyopel <codyopel@gmail.com>
2014-10-31 08:28:05 -0400

    nixpkgs/config.nix: refactored :fu::shipit:

--- dotfiles/nixpkgs/config.nix
+++ dotfiles/nixpkgs/config.nix
@@ -11,155 +11,191 @@ pkgs : {
     enablePepperFlash = true;
     enablePepperPDF = true;
   };
+  # FFmpeg - enable fdk-aac
   ffmpeg.fdk = true;
+  # Suckless
   st.conf = (builtins.readFile ./st/config.mach.h)
-    + (builtins.readFile ./st/config.inc.h);  
+    + (builtins.readFile ./st/config.inc.h);
+
   packageOverrides = self : rec {
     rtorrent-git = self.rtorrent-git.override { colorSupport = true; };
-    hsEnv = self.haskellPackages.ghcWithPackages (self : with self; [
-      xmonad
-      #yi
-    ]);
-    graphical = self.buildEnv {
-      name = "myGraphical";
+    # Import Environments
+    chlorm = self.buildEnv {
+      name = "myChlorm";
       paths = with self; [
-        # Envs
-        #cs225
-        #dev
-        #orpheum
-        #shell
-        #profile
-        #mumc
-
-        # Pkgs
-        bc
-        pythonPackages.beets
-        chromium
-        conky
-        dmenu
-        dzen2
-        emacs
-        ffmpeg
-        firefoxWrapper
-        filezilla
-        flac
-        gimp
-        #gnupg1compat
-        hsEnv
-        #icedtea7_web
-        lame
-        #libreoffice
-        ##mediainfo
-        mkvtoolnix
-        most
-        mpd
-        mumble
-        ncdc
-        ncdu
-        ncmpcpp
-        networkmanager
-        networkmanagerapplet
-        #nix-repl
-        #nixops
-        #notbit
-        p7zip
-        pavucontrol
-        pcsclite
-        pidgin
-        pinentry
-        pulseaudio
-        #kde4.quasselClient
-        qbittorrent
-        rtorrent-git
-        sakura
-        scrot
-        #sl
-        speedtest_cli
-        #st
-        sublime3
-        #sup
-        texLive
-        texstudio
-        transmission
-        #virtmanager
-        vlc
-        vobsub2srt
-        haskellPackages.xdgBasedir
-        x264
-        #xlibs.xbacklight
-        youtubeDL
-        #zathura
+        # Required
+        base
+        # Optional
+        audio
+        communication
+        desktop
+        development
+        download
+        editors
+        haskell
+        headless
+        image
+        monitoring
+        networking
+        shells
+        terminals
+        video
+        virtualization
+        www
       ];
     };
-    profile = self.myEnvFun {
-      name = "profile";
-      buildInputs = with self; [
-        (self.haskellPackages.ghcWithPackages (self : with self; [
-          hakyll
-        ]))
-      ];
-    };
-    nongraphical = self.buildEnv {
-      name = "myNonGraphical";
+    # Environments
+    base = self.buildEnv {
+      name = "myBase";
       paths = with self; [
-        #dev
-        shell
-      ];
-    };
-    dev = self.buildEnv {
-      name = "myDev";
-      paths = with self; [
-        cdrkit
-        #python3Packages.ipython
-        subversion
-      ];
-    };
-    shell = self.buildEnv {
-      name = "myShell";
-      paths = with self; [
-        acpi
-        fish
         git
         htop
-        mosh
         openssh_hpn
         openssl
-        psmisc
         tmux
-        unzip
-        vim
         wget
       ];
     };
-    mumc = self.myEnvFun {
-      name = "mumble-connect";
-      buildInputs = with self; [
-        stdenv
-        autoconf
-        automake
-        libtool
-        pkgconfig
-        valgrind
+    audio = self.buildEnv {
+      name = "myAudio";
+      paths = with self; [
+        pythonPackages.beets
+        flac
+        lame
+        mpd
+        ncmpcpp
+        pavucontrol
+        pulseaudio
       ];
     };
-    cs225 = self.myEnvFun {
-      name = "cs225";
-      buildInputs = with self; [
-        gdb
+    communication = self.buildEnv {
+      name = "myCommunication";
+      paths = with self; [
+        mumble
+        pidgin
+        #kde4.quasselClient
+      ];
+    };
+    desktop = self.haskellPackages.ghcWithPackages (self : with self; [ xmonad ]);
+    development = self.buildEnv {
+      name = "myDevelopment";
+      paths = with self; [
+        #icedtea7_web
+        #nix-repl
+        #nixops
+        subversion
+        texLive
+      ];
+    };
+    download = self.buildEnv {
+      name = "myDownload";
+      paths = with self; [
+        filezilla
+        ncdc
+        qbittorrent
+        rtorrent-git
+        transmission
+        youtubeDL
+      ];
+    };
+    editors = self.buildEnv {
+      name = "myEditors";
+      paths = with self; [
+        atom
+        emacs
+        #libreoffice
+        sublime3
+        texstudio
+        vim
+      ];
+    };
+    haskell = self.buildEnv {
+      name = "myHaskell";
+      paths = with self; [
+        haskell.packages_ghc763
+        haskellPackages.xdgBasedir
+      ];
+    };
+    headless = self.buildEnv {
+      name = "myHeadless";
+      paths = with self; [
+        acpi
+        bc
+        conky
+        dmenu
+        dzen2
+        #gnupg1compat
+        mosh
+        most
+        #notbit
+        p7zip
+        pcsclite
+        pinentry
+        psmisc
+        scrot
+        unzip
+        #xlibs.xbacklight
+        #zathura
+      ];
+    };
+    image = self.buildEnv {
+      name = "myImage";
+      paths = with self; [
+        gimp
         imagemagick
         libpng
-        stdenv
-        valgrind
       ];
     };
-    orpheum = self.myEnvFun {
-      name = "orpheum";
-      buildInputs = (with self; [ python27
-      #rubyLibs.sass_3_3_4
-      ])
-        ++ (with self.python27Packages; [
-          django_1_5 google_api_python_client paypalrestsdk pil sorl_thumbnail six sqlite3
-        ]);
+    monitoring = self.buildEnv {
+      name = "myMonitoring";
+      paths = with self; [
+        ncdu
+        speedtest_cli
+      ];
+    };
+    networking = self.buildEnv {
+      name = "myNetworking";
+      paths = with self; [
+        networkmanager
+        networkmanagerapplet
+      ];
+    };
+    shells = self.buildEnv {
+      name = "myShells";
+      paths = with self; [
+        fish
+        #zsh
+      ];
+    };
+    terminals = self.buildEnv {
+      name = "myTerminals";
+      paths = with self; [
+        sakura
+      ];
+    };
+    video = self.buildEnv {
+      name = "myVideo";
+      paths = with self; [
+        ffmpeg
+        #mediainfo
+        mkvtoolnix
+        vlc
+        vobsub2srt
+        x264
+      ];
+    };
+    virtualization = self.buildEnv {
+      name = "myVirtualization";
+      paths = with self; [
+        virtmanager
+      ];
+    };
+    www = self.buildEnv {
+      name = "myWww";
+      paths = with self; [
+        chromium
+        firefoxWrapper
+      ];
     };
   };
 }
