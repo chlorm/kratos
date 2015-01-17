be3a92243273db284a79877ab3616a5d6be079a8
codyopel <codyopel@gmail.com>
2015-01-17 14:34:40 -0500

    path_basbin(): add generic error message

--- lib/fixes.bash
+++ lib/fixes.bash
@@ -1,33 +1,52 @@
 #!/usr/bin/env bash
 
-##[creates an array from the name of the variable and values]
+# Creates an array from the name of the variable and values
 array_from_str () {
+
   ([ "$#" -lt "1" ] || [ "$#" -gt "2" ]) && return 1
-  ##[read in the array data]
-  local DATA;
-  if [ "$#" -eq "1" ];then DATA="$(cat -)"
-  else DATA="$2";fi
-  ##[convert the data into an array]
+
+  # Read in the array data
+  local DATA
+
+  if [ "$#" -eq "1" ];then
+    DATA="$(cat -)"
+  else
+    DATA="$2"
+  fi
+
+  # Convert the data into an array
   eval "$1=($DATA)"
+
 }
 
-##[unique array functions with indicies starting at 0]
+# Unique array functions with indicies starting at 0
 array_append () {
+
   eval "$1=(\"\${$1[@]}\" \"$2\")"
+
 }
 array_new () {
+
   unset "$1"
   declare -a "$1"
+
 }
 array_at () {
+
   eval "echo \"\${$1[$2]}\""
+
 }
 array_size () {
+
   eval "echo \${#$1[@]}"
+
 }
 
-##[tests to see if a binary exists in the path]
+# Test to see if a binary exists in the path
 path_hasbin () {
+
   [ "$#" -ne "1" ] && return 2
-  type $1 >/dev/null 2>&1
+  type $1 >/dev/null 2>&1 || \
+  { echo "'$1' not installed" ; }
+
 }
--- lib/fixes.zsh
+++ lib/fixes.zsh
@@ -2,32 +2,54 @@
 
 ##[creates an array from the name of the variable and values]
 array_from_str () {
+
   {[ "$#" -lt "1" ] || [ "$#" -gt "2" ]} && return 1
+
   ##[read in the array data]
-  local DATA;
-  if [ "$#" -eq "1" ];then DATA="$(cat -)"
-  else DATA="$2";fi
+  local DATA
+
+  if [ "$#" -eq "1" ] ; then
+    DATA="$(cat -)"
+  else
+    DATA="$2"
+  fi
+
   ##[convert the data into an array]
   eval "$1=($DATA)"
+
 }
 
 ##[unique array functions with indicies starting at 0]
 array_new () {
+
   unset "$1"
   declare -a "$1"
+
 }
+
 array_at () {
+
   eval "echo \${$1[$(expr $2 + 1)]}"
+
 }
+
 array_append () {
+
   eval "$1+=(\"$2\")"
+
 }
+
 array_size () {
+
   eval "echo \${#$1}"
+
 }
 
 ##[tests to see if a binary exists in the path]
 path_hasbin () {
+
   [ "$#" -ne "1" ] && return 2
-  whence -p $1 >/dev/null 2>&1
+  whence -p $1 >/dev/null 2>&1 || \
+  { echo "'$1' not installed" ; }
+
 }
