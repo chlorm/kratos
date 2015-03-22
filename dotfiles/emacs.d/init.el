f06c2d7db42f3f9f772ad8b7a43ee180014d9a1e
codyopel <codyopel@gmail.com>
2015-03-22 05:34:30 -0400

    dotfiles/emacs: remove unused portion of config

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -11,10 +11,3 @@
 (setq linum-format " %d ") ; Pad space around numbers
 
 (column-number-mode 1)
-
-(setq default-frame-alist
-      (append default-frame-alist
-       '((foreground-color . "Black")
- (background-color . "White")
- (cursor-color . "SkyBlue")
- )))
