9ddc8f6f12a75421f359d14c0cfaa3c863cc2267
codyopel <codyopel@gmail.com>
2015-07-08 12:11:32 -0400

    add test to required shell features

--- tests/supported-shell.sh
+++ tests/supported-shell.sh
@@ -1,6 +1,17 @@
+# This file is part of Kratos.
+# Copyright (c) 2014-2015, Cody Opel <codyopel@gmail.com>.
+#
+# Use of this source code is governed by the terms of the
+# BSD-3 license.  A copy of the license can be found in
+# the `LICENSE' file in the top level source directory.
+
+# This should include tests for anything used that is not part of the POSIX spec
+# to test shell compatibility with Kratos.
+
+# BASH style arrays are used throughout Kratos
 shell_array_test() {
 
-  TEST_FOR_ARRAY_SUPPORT=("823")
+  local TEST_FOR_ARRAY_SUPPORT=("823")
 
   if [ "${TEST_FOR_ARRAY_SUPPORT[0]}" -eq 823 ] ; then
     return 0
@@ -10,3 +21,12 @@ shell_array_test() {
 
 }
 shell_array_test
+
+# Kratos uses the `function <function name>' method to declare functions
+# instead of the Bourne/POSIX `<function name>()' method
+function function_definition_test {
+
+  return 0
+
+}
+function_definition_test
