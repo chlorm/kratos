4e4c855e374cc3af2a3b2a32230efa3068e09531
Cody Opel <codyopel@gmail.com>
2015-07-16 03:51:52 -0400

    modules/golang: add init & dir creation

--- modules/golang/main.sh
+++ modules/golang/main.sh
@@ -5,14 +5,47 @@
 # BSD-3 license.  A copy of the license can be found in
 # the `LICENSE' file in the top level source directory.
 
-function golang {
+function GoLangDirs {
+
+  EnsureDirExists "${HOME}/Dev/go/bin" || return 1
+  EnsureDirExists "${HOME}/Dev/go/pkg" || return 1
+  EnsureDirExists "${HOME}/Dev/go/src" || return 1
+
+  return 0
+
+}
+
+function GoLangBinPath {
+
   if [[ -n "${GOPATH}" && "${GOPATH}" != "${HOME}/Dev/go" ]] ; then
-    # Probably need to sanitize $GOPATH in case if contains multiple PATHs
+    # TODO: sanitize $GOPATH in case it contains multiple PATHs
     # Output paths to an array and iterate throught the array elements
-    PathAdd "${GOPATH}/bin"
+    PathAdd "${GOPATH}/bin" || {
+      ErrWarn 'failed to configure $GOPATH'
+      return 1
+    }
     export GOPATH="${GOPATH}"
   elif [ -d "${HOME}/Dev/go" ] ; then
-    PathAdd "${HOME}/Dev/go/bin"
+    PathAdd "${HOME}/Dev/go/bin" || {
+      ErrWarn 'failed to configure $GOPATH'
+      return 1
+    }
     export GOPATH="${HOME}/Dev/go"
   fi
+
+  return 0
+
+}
+
+function GoLangGoPath {
+
+  if [[ -n "${GOPATH}" ]] ; then
+    # TODO: add tests to make sure dirs in $GOPATH exist
+    export GOPATH="${GOPATH}"
+  else
+    export GOPATH="${HOME}/Dev/go"
+  fi
+
+  return 0
+
 }
