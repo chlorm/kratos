ce1c2ce66ae194ad29b9f592ec02e4617f156eb3
Cody Opel <codyopel@gmail.com>
2015-07-17 16:07:47 -0400

    Add default config file

---
+++ lib/DEFAULTS.sh
@@ -0,0 +1,26 @@
+# This file is part of Kratos.
+# Copyright (c) 2014-2015, Cody Opel <codyopel@gmail.com>.
+#
+# Use of this source code is governed by the terms of the
+# BSD-3 license.  A copy of the license can be found in
+# the `LICENSE' file in the top level source directory.
+
+# ~/Projects
+KRATOS_CREATE_PROJECT_DIRECTORIES=true
+
+# ~/.local/share/trash/files
+# ~/.local/share/trash/info
+KRATOS_CREATE_TRASH_DIRECTORIES=true
+
+# ~/.cache"       # XDG_CACHE_HOME
+# ~/.config"      # XDG_CONFIG_HOME
+# ~/.local/share" # XDG_DATA_HOME
+# ~/Desktop"      # XDG_DESKTOP_DIR
+# ~/Documents"    # XDG_DOCUMENTS_DIR
+# ~/Downloads"    # XDG_DOWNLOAD_DIR
+# ~/Music"        # XDG_MUSIC_DIR
+# ~/Pictures"     # XDG_PICTURES_DIR
+# ~/Share"        # XDG_PUBLICSHARE_DIR
+# ~/Templates"    # XDG_TEMPLATES_DIR
+# ~/Videos"       # XDG_VIDEOS_DIR
+KRATOS_CREATE_XDG_DIRECTORIES=true
