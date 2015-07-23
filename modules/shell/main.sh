208a58e9da421cc892b49d5f1ddc8bcb47287342
Cody Opel <codyopel@gmail.com>
2015-07-23 04:23:52 -0400

    modules/shell: remove duplicate aliases

--- modules/shell/main.sh
+++ modules/shell/main.sh
@@ -46,9 +46,6 @@ function ShellInit { # Initializes useful functions
   alias sprunge="curl -F 'sprunge=<-' http://sprunge.us"
   alias nixpaste="curl -F 'text=<-' http://nixpaste.noip.me"
   #alias t='laptop_bat ; date'
-  alias sha1="openssl sha1"
-  alias sha256="openssl sha256"
-  alias sha512="openssl sha512"
   alias youtube="youtube-dl --max-quality --no-check-certificate --prefer-insecure --console-title"
   alias music="ncmpcpp"
   alias tarxz="tar cjvf"
@@ -71,13 +68,11 @@ function ShellInit { # Initializes useful functions
   else
     export EDITOR="${PREFERED_EDITOR}"
   fi
-  export PAGER="less -R"
   export BLOCKSIZE="K"
   # Locale/UTF-8
   export LANG=en_US.UTF-8
   export LANGUAGE=en_US.UTF-8
   export LC_ALL=en_US.UTF-8
-  export LESSCHARSET=utf-8
   export PERL_UTF8_LOCALE=1
   export PERL_UNICODE=AS
   # Prevent GUI password dialog
