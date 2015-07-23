9eff65af3d7a87bc66f7ffb88ffef91347f81e70
Cody Opel <codyopel@gmail.com>
2015-07-23 04:20:48 -0400

    plugins/base64: add functions

--- plugins/base64/main.sh
+++ plugins/base64/main.sh
@@ -4,3 +4,25 @@
 # Use of this source code is governed by the terms of the
 # BSD-3 license.  A copy of the license can be found in
 # the `LICENSE' file in the top level source directory.
+
+function encode64 {
+
+  [[ -n ${1+x} ]] || {
+    ErrError 'no input'
+    return 1
+  }
+
+  echo -n ${1} | base64
+
+}
+
+function decode64 {
+
+  [[ -n ${1+x} ]] || {
+    ErrError 'no input'
+    return 1
+  }
+
+  echo -n ${1} | base64 --decode
+
+}
