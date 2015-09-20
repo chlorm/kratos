8718d5fcbaeff700b239b3cc614754a3e7974392
Cody Opel <codyopel@gmail.com>
2015-09-20 00:53:43 -0400

    move to correct module

--- modules/terminal/main.sh
+++ modules/terminal/main.sh
@@ -4,21 +4,3 @@
 # Use of this source code is governed by the terms of the
 # BSD-3 license.  A copy of the license can be found in
 # the `LICENSE' file in the top level source directory.
-
-# TODO:
-# + Set the TERM environment variable to allow color support if terminal
-#    supports color
-# + Make sure the terminal module is executed before color-conv
-
-# dumb, screen, xterm, vt100, color, ansi, cygwin, linux
-
-# if isTTY, then try fbterm else false
-
-function TermClrs {
-if [ "${TERM}" == 'xterm' ] ; then
-  # See if xterm supports 256 color
-  if [ -n "${VTE_VERSION}" ] ; then
-    export TERM='xterm-256color'
-  fi
-fi
-}
