111228edbdbdafca546f93f08023a280685f02c7
Cody Opel <codyopel@gmail.com>
2015-09-20 00:51:01 -0400

    Move portions of modules/shell to correct modules

--- modules/locale/main.sh
+++ modules/locale/main.sh
@@ -4,3 +4,10 @@
 # Use of this source code is governed by the terms of the
 # BSD-3 license.  A copy of the license can be found in
 # the `LICENSE' file in the top level source directory.
+
+# Locale/UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export PERL_UTF8_LOCALE=1
+export PERL_UNICODE=AS
