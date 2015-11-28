babb0bcd6dc3e702bb8bef2a1be24a4e90c15dd4
Cody Opel <codyopel@gmail.com>
2015-11-28 12:31:51 -0500

    add zsh history config options

--- modules/history/main.sh
+++ modules/history/main.sh
@@ -30,6 +30,23 @@ function HistoryShell {
 
 }
 
+# Show history
+case "${HIST_STAMPS}" in
+  'mm/dd/yyyy') alias history='fc -fl 1' ;;
+  'dd.mm.yyyy') alias history='fc -El 1' ;;
+  'yyyy-mm-dd') alias history='fc -il 1' ;;
+  *) alias history='fc -l 1' ;;
+esac
+
+setopt append_history
+setopt extended_history
+setopt hist_expire_dups_first
+setopt hist_ignore_dups # ignore duplication command history list
+setopt hist_ignore_space
+setopt hist_verify
+setopt inc_append_history
+setopt share_history # share command history data
+
 alias h='history'
 
 # Not sure what this crap does
