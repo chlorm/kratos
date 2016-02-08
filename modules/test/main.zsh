f1a946685d97ef2a509d0bdf5253fcfe53edbd21
Cody Opel <codyopel@gmail.com>
2016-02-08 17:33:54 -0500

    add spinning cursor

--- modules/test/main.zsh
+++ modules/test/main.zsh
@@ -2,3 +2,14 @@ asdf() {
 
   PathHasBinErr 'asdff'
 }
+
+spin() {
+
+  local -a marks=( '/' '-' '\' '|' ' ' )
+
+  while [[ 1 ]] ; do
+    printf '%s\r' "${marks[i++ % ${#marks[@]}]}"
+    sleep 0.2
+  done
+
+}
