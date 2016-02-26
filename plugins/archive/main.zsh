26e15f3faa7a182b70846fd96ccff09e0d0315d4
Cody Opel <codyopel@gmail.com>
2016-02-26 04:28:47 -0500

    plugins/archive: refactor

--- plugins/archive/main.zsh
+++ plugins/archive/main.zsh
@@ -1,88 +1,73 @@
 # This file is part of Kratos.
-# Copyright (c) 2014-2015, Cody Opel <codyopel@gmail.com>.
+# Copyright (c) 2014-2016, Cody Opel <codyopel@gmail.com>.
 #
 # Use of this source code is governed by the terms of the
 # BSD-3 license.  A copy of the license can be found in
 # the `LICENSE' file in the top level source directory.
 
-KRATOS::Plugins:archive.usage() {
-  echo
-}
-
-KRATOS::Plugins:archive.command() {
-  case "$1" in
-    '-c')
-      ;;
-    '-e')
-      ;;
-    '-l')
-      ;;
-  esac
-}
-
 KRATOS::Plugins:archive.extract() {
-  if [[ -z "$1" ]] ; then
-    KRATOS::Lib:err.error "no input provided"
-    exit 1
-  elif [[ -f "$1" ]] ; then
-    case "$1" in
+  if [[ -z "$@" ]] ; then
+    KRATOS::Lib:err.error 'no input provided'
+    return 1
+  elif [[ -f "$@" ]] ; then
+    case "$@" in
       *'.tar')
         KRATOS::Lib:path.has_bin tar || return 1
-        tar xf "$1"
+        tar xvf "$@" || return 2
         ;;
       *'.tar.bz2')
-        KRATOS::Lib:path.has_bin tar tar || return 1
-        tar xjf "$1"
+        KRATOS::Lib:path.has_bin tar || return 1
+        tar xjvf "$@" || return 2
         ;;
       *'.tbz2')
-        KRATOS::Lib:path.has_bin tar tar || return 1
-        tar xjf "$1"
+        KRATOS::Lib:path.has_bin tar || return 1
+        tar xjvf "$@" || return 2
         ;;
       *'.bz2')
-        KRATOS::Lib:path.has_bin tar bunzip2 || return 1
-        bunzip2 "$1"
+        KRATOS::Lib:path.has_bin bunzip2 || return 1
+        bunzip2 "$@" || return 2
         ;;
       *'.tar.gz')
-        KRATOS::Lib:path.has_bin tar tar || return 1
-        tar xzf "$1"
+        KRATOS::Lib:path.has_bin tar || return 1
+        tar xzvf "$@" || return 2
         ;;
       *'.tgz')
-        KRATOS::Lib:path.has_bin tar tar || return 1
-        tar xzf "$1"
+        KRATOS::Lib:path.has_bin tar || return 1
+        tar xzvf "$@" || return 2
         ;;
       *'.gz')
-        KRATOS::Lib:path.has_bin tar gunzip || return 1
-        gunzip "$1"
+        KRATOS::Lib:path.has_bin gunzip || return 1
+        gunzip "$@" || return 2
         ;;
       *'.tar.xz')
-        KRATOS::Lib:path.has_bin tar tar || return 1
-        tar xJf "$1"
+        KRATOS::Lib:path.has_bin tar || return 1
+        tar xJvf "$@" || return 2
         ;;
       *'.txz')
-        KRATOS::Lib:path.has_bin tar tar || return 1
-        tar xJf "$1"
+        KRATOS::Lib:path.has_bin tar || return 1
+        tar xJvf "$@" || return 2
         ;;
       *'.rar')
-        KRATOS::Lib:path.has_bin tar unrar || return 1
-        unrar e "$1"
+        KRATOS::Lib:path.has_bin unrar || return 1
+        unrar e "$@" || return 2
         ;;
       *'.zip')
-        KRATOS::Lib:path.has_bin tar unzip || return 1
-        unzip "$1"
+        KRATOS::Lib:path.has_bin unzip || return 1
+        unzip "$@" || return 2
         ;;
       *'.Z')
-        KRATOS::Lib:path.has_bin tar uncompress || return 1
-        uncompress "$1"
+        KRATOS::Lib:path.has_bin uncompress || return 1
+        uncompress "$@" || return 2
         ;;
       *'.7z')
-        KRATOS::Lib:path.has_bin tar 7z || return 1
-        7z x "$1"
+        KRATOS::Lib:path.has_bin 7z || return 1
+        7z x "$@" || return 2
         ;;
       *)
-        KRATOS::Lib:err.error "'$1' is not a supported file type"
+        KRATOS::Lib:err.error "'$@' is not a supported file type"
         ;;
       esac
   else
-      KRATOS::Lib:err.error "'$1' is not a file"
+      KRATOS::Lib:err.error "'$@' is not a file"
   fi
 }
