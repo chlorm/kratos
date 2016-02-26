86629ec363410059ae6408c3892562b0b074d5c3
Cody Opel <codyopel@gmail.com>
2016-02-26 14:17:59 -0500

    move completion options to modules/completion

--- modules/completion/main.zsh
+++ modules/completion/main.zsh
@@ -5,6 +5,20 @@
 # BSD-3 license.  A copy of the license can be found in
 # the `LICENSE' file in the top level source directory.
 
+# When completing from the middle of a word,
+# move the cursor to the end of the word.
+setopt always_to_end
+# show completion menu on successive tab press.
+# needs unsetop menu_complete to work
+setopt auto_menu
+# any parameter that is set to the absolute name of a
+# directory immediately becomes a name for that directory
+setopt auto_name_dirs
+# Allow completion from within a word/phrase
+setopt complete_in_word
+# do not autoselect the first completion entry
+unsetopt menu_complete
+
 autoload -U compinit
 compinit
 
