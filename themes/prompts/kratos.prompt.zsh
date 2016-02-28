7d88530199a7eb81fc880bba7f239f010022718d
Cody Opel <codyopel@gmail.com>
2016-02-28 16:26:35 -0500

    prompt/kratos: use light color alternative

--- themes/prompts/kratos.prompt.zsh
+++ themes/prompts/kratos.prompt.zsh
@@ -5,7 +5,7 @@
 # BSD-3 license.  A copy of the license can be found in
 # the `LICENSE' file in the top level source directory.
 
-KRATOS_PROMPT_1='$(kprmt f3)%n$(kprmt bold)$(kprmt f1)@$(kprmt bold)$(kprmt f16)%M$(kprmt f1)[$(kprmt reset)$(kprmt f2)%~$(kprmt bold)$(kprmt f1)]$(kprmt f7)〉$(kprmt reset)'
+KRATOS_PROMPT_1='$(kprmt f11)%n$(kprmt bold)$(kprmt f1)@$(kprmt bold)$(kprmt f16)%M$(kprmt f1)[$(kprmt reset)$(kprmt f2)%~$(kprmt bold)$(kprmt f1)]$(kprmt f7)〉$(kprmt reset)'
 KRATOS_PROMPT_2=''
 KRATOS_PROMPT_3=''
 KRATOS_PROMPT_4=''
