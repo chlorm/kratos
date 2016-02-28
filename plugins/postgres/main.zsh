3af7da8277e0fe9d4ea173a33f45d95b834d0d2a
Cody Opel <codyopel@gmail.com>
2016-02-28 05:59:24 -0500

    remove imcomplete placeholder plugins

--- plugins/postgres/main.zsh
+++
@@ -1,8 +0,0 @@
-# This file is part of Kratos.
-# Copyright (c) 2014-2015, Cody Opel <codyopel@gmail.com>.
-#
-# Use of this source code is governed by the terms of the
-# BSD-3 license.  A copy of the license can be found in
-# the `LICENSE' file in the top level source directory.
-
-# Wrapper, backups, aliases
