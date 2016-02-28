88ae0047c757ee9e05d89adce2fcb80d00340fd8
Cody Opel <codyopel@gmail.com>
2016-02-28 16:27:53 -0500

    color-scheme/gruvbox: add color scheme

---
+++ themes/color-schemes/gruvbox.color-scheme.zsh
@@ -0,0 +1,25 @@
+# This file is part of Kratos.
+# Copyright (c) 2016, Cody Opel <codyopel@gmail.com>.
+#
+# Use of this source code is governed by the terms of the
+# BSD-3 license.  A copy of the license can be found in
+# the `LICENSE' file in the top level source directory.
+
+# https://github.com/morhetz/gruvbox
+
+KRATOS_COLOR_1='282828' # Black
+KRATOS_COLOR_2='cc241d' # Red
+KRATOS_COLOR_3='98971a' # Green
+KRATOS_COLOR_4='d79921' # Yellow
+KRATOS_COLOR_5='458588' # Blue
+KRATOS_COLOR_6='b16286' # Magenta
+KRATOS_COLOR_7='689d6a' # Cyan
+KRATOS_COLOR_8='a89984' # Light gray
+KRATOS_COLOR_9='928374' # Dark gray
+KRATOS_COLOR_10='fb4934' # Light red
+KRATOS_COLOR_11='b8bb26' # Light green
+KRATOS_COLOR_12='fabd2f' # Light yellow
+KRATOS_COLOR_13='83a598' # Light blue
+KRATOS_COLOR_14='d3869b' # Light magenta
+KRATOS_COLOR_15='8ec07c' # Light cyan
+KRATOS_COLOR_16='ebdbb2' # White
