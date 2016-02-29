f5d3137ccb00bc0b603daefaaa1bae19336b8234
Cody Opel <codyopel@gmail.com>
2016-02-29 00:40:54 -0500

    fix copyright

--- lib/pre-flight-checks.zsh
+++ lib/pre-flight-checks.zsh
@@ -1,3 +1,9 @@
+# This file is part of Kratos.
+# Copyright (c) 2015-2016, Cody Opel <codyopel@gmail.com>.
+#
+# Use of this source code is governed by the terms of the
+# BSD-3 license.  A copy of the license can be found in
+# the `LICENSE' file in the top level source directory.
 
 KRATOS::Lib:path.has_installed() {
   echo "PathHasBin$(
--- plugins/triton/main.zsh
+++ plugins/triton/main.zsh
@@ -1,5 +1,5 @@
 # This file is part of Kratos.
-# Copyright (c) 2014-2015, Cody Opel <codyopel@gmail.com>.
+# Copyright (c) 2016, Cody Opel <codyopel@gmail.com>.
 #
 # Use of this source code is governed by the terms of the
 # BSD-3 license.  A copy of the license can be found in
