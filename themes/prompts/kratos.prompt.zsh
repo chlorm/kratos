cf20f192cd5ce874ac0350463dbb67f481d71782
Cody Opel <codyopel@gmail.com>
2016-02-29 00:40:19 -0500

    prompts/kratos: change hostname color if connected via ssh

--- themes/prompts/kratos.prompt.zsh
+++ themes/prompts/kratos.prompt.zsh
@@ -5,7 +5,13 @@
 # BSD-3 license.  A copy of the license can be found in
 # the `LICENSE' file in the top level source directory.
 
-KRATOS_PROMPT_1='$(kprmt f11)%n$(kprmt bold)$(kprmt f1)@$(kprmt bold)$(kprmt f16)%M$(kprmt f1)[$(kprmt reset)$(kprmt f2)%~$(kprmt bold)$(kprmt f1)]$(kprmt f7)〉$(kprmt reset)'
+if [[ -n "${SSH_CLIENT}" ]] ; then
+  KratosPromptSsh='f10'
+else
+  KratosPromptSsh='f16'
+fi
+
+KRATOS_PROMPT_1='$(kprmt f11)%n$(kprmt bold)$(kprmt f1)@$(kprmt bold)$(kprmt ${KratosPromptSsh})%M$(kprmt f1)[$(kprmt reset)$(kprmt f2)%~$(kprmt bold)$(kprmt f1)]$(kprmt f7)〉$(kprmt reset)'
 KRATOS_PROMPT_2=''
 KRATOS_PROMPT_3=''
 KRATOS_PROMPT_4=''
