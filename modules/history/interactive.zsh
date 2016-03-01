b0a3b61bd22b09c7f3c72d2c6591c4a192597c17
Cody Opel <codyopel@gmail.com>
2016-02-29 21:20:54 -0500

    modules/history: move aliases to inteactive

--- modules/history/interactive.zsh
+++ modules/history/interactive.zsh
@@ -7,6 +7,22 @@
 
 KRATOS::Modules:history.shell
 
+# Show history
+case "${HIST_STAMPS}" in
+  'mm/dd/yyyy')
+    alias history='fc -fl 1'
+    ;;
+  'dd.mm.yyyy')
+    alias history='fc -El 1'
+    ;;
+  'yyyy-mm-dd')
+    alias history='fc -il 1'
+    ;;
+  *)
+    alias history='fc -l 1'
+    ;;
+esac
+
 alias h='history'
 alias hs='KRATOS::Modules:history.command'
 alias hsi='hs -i'
