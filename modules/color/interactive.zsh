e8feafe087e947ab22b503579beecfdcd246a9ce
Cody Opel <codyopel@gmail.com>
2016-02-29 21:35:27 -0500

    modules/color: move color variables to function

--- modules/color/interactive.zsh
+++ modules/color/interactive.zsh
@@ -7,55 +7,6 @@
 
 KRATOS::Modules:color.ls
 
+KRATOS::Modules:color.define_colors
+
 KRATOS::Modules:color.set_scheme
-
-KCLR_FG_1='\e[30m' # Black
-KCLR_FG_2='\e[31m' # Red
-KCLR_FG_3='\e[32m' # Green
-KCLR_FG_4='\e[33m' # Yellow
-KCLR_FG_5='\e[34m' # Blue
-KCLR_FG_6='\e[35m' # Magenta
-KCLR_FG_7='\e[36m' # Cyan
-KCLR_FG_8='\e[37m' # Light gray
-KCLR_FG_9='\e[90m' # Dark gray
-KCLR_FG_10='\e[91m' # Light red
-KCLR_FG_11='\e[92m' # Light green
-KCLR_FG_12='\e[93m' # Light yellow
-KCLR_FG_13='\e[94m' # Light blue
-KCLR_FG_14='\e[95m' # Light magenta
-KCLR_FG_15='\e[96m' # Light cyan
-KCLR_FG_16='\e[97m' # White
-KCLR_FG_RESET='\e[39m' # Reset foreground
-
-KCLR_BG_1='\e[40m' # Black
-KCLR_BG_2='\e[41m' # Red
-KCLR_BG_3='\e[42m' # Green
-KCLR_BG_4='\e[44m' # Yellow
-KCLR_BG_5='\e[44m' # Blue
-KCLR_BG_6='\e[45m' # Magenta
-KCLR_BG_7='\e[46m' # Cyan
-KCLR_BG_8='\e[47m' # Light gray
-KCLR_BG_9='\e[100m' # Dark gray
-KCLR_BG_10='\e[101m' # Light red
-KCLR_BG_11='\e[102m' # Light green
-KCLR_BG_12='\e[103m' # Light yellow
-KCLR_BG_13='\e[104m' # Light blue
-KCLR_BG_14='\e[105m' # Light magenta
-KCLR_BG_15='\e[106m' # Light cyan
-KCLR_BG_16='\e[107m' # White
-KCLR_BG_RESET='\e[49m' # Reset background
-
-KCLR_BOLD='\e[1m'
-KCLR_DIM='\e[2m'
-KCLR_UNDERLINE='\e[4m'
-KCLR_BLINK='\e[5m'
-KCLR_INVERT='\e[7m'
-KCLR_HIDDEN='\e[8m'
-
-KCLR_RESET='\e[0m'
-KCLR_RESET_BOLD='\e[21m'
-KCLR_RESET_DIM='\e[22m'
-KCLR_RESET_UNDERLINE='\e[24m'
-KCLR_RESET_BLINK='\e[25m'
-KCLR_RESET_INVERT='\e[27m'
-KCLR_RESET_HIDDEN='\e[28m'
