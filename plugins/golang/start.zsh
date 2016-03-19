ffee0f836392111a952c6216db1c25fc2f1ce000
Cody Opel <codyopel@gmail.com>
2016-03-19 04:52:24 -0400

    plugins/golang: only checkout for directories on start

--- plugins/golang/start.zsh
+++ plugins/golang/start.zsh
@@ -1,12 +1,10 @@
 # This file is part of Kratos.
-# Copyright (c) 2014-2016, Cody Opel <codyopel@gmail.com>.
+# Copyright (c) 2016, Cody Opel <codyopel@gmail.com>.
 #
 # Use of this source code is governed by the terms of the
 # BSD-3 license.  A copy of the license can be found in
 # the `LICENSE' file in the top level source directory.
 
 if ${PathHasBinGO} ; then
-  KRATOS::Plugins:golang.bin_path
-
-  KRATOS::Plugins:golang.gopath
+  KRATOS::Plugins:golang.directories
 fi
