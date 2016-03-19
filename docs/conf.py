61bad6bed0ffd2759ee9500dbe69a4d534f8eed0
Cody Opel <codyopel@gmail.com>
2016-03-19 07:44:25 -0400

    docs: add sphinx config

---
+++ docs/conf.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+project = u'Kratos'
+copyright = u'2014-2016, Cody Opel. and contributors. Licensed under Creative Commons BY-SA'
+
+source_suffix = '.rst'
+master_doc = 'index'
+
+html_theme = 'default'
