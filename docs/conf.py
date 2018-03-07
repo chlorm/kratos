14703969dac81fd2fce7a7607efa4ddcace2336e
Cody W. Opel <codyopel@gmail.com>
2018-03-06 23:10:00 -0500

    remove legacy docs

--- docs/conf.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-project = u'Kratos'
-copyright = u'2014-2016, Kratos and contributors. Licensed under Creative Commons BY-SA'
-
-source_suffix = '.rst'
-master_doc = 'index'
-
-html_theme = 'default'
--- docs/install.rst
+++
@@ -1,9 +0,0 @@
-*******
-Install
-*******
-
-.. code-block:: shell
-
-  git clone 'https://github.com/chlorm/kratos.git' "${HOME}/.kratos"
-  cd "${HOME}/.kratos"
-  bash ./bootstrap.bash
